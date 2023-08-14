5eeaab1bb32e34870a2aa68c01cd2e5623bc4a35
student2 <student2@itpark2.local>
2023-08-14 18:41:51 +0500

    prosta tak

---
+++ ls3_random.py
@@ -0,0 +1,26 @@
+from random import *
+
+mevalar = [
+    "olma","banan","nok",
+           "shaftoli","kivi","apelsin"
+]
+
+print(choice(mevalar))
+# print(sample(mevalar,k=2))
+# shuffle(mevalar)
+# print(mevalar)
+
+
+def choice_2(royxat):
+    return royxat[randint(0,len(royxat) - 1)]
+
+def shuffle_2(data):
+    pass
+
+
+# print(shuffle_2(mevalar))
+
+
+
+
+
